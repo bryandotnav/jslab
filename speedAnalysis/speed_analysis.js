e650cd0661e2b4147afd7223a343dc80cd27c0c7
Bryan <bryan001700@gmail.com>
2025-12-02 15:55:13 -0500

    added a function that generates random text

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -1,7 +1,29 @@
+let wordsArray = [];
+fetch('words.txt')
+  .then(response => response.text())
+  .then(data => {
+    wordsArray = data.split('\n'); // or split by whatever separator you use
+  });
+  /*.catch((error)=>{
+    console.error("caught in error chain:", error.message);
+  });*/
+  function generateText() {
+    let numberOfWords = 30; // amount of words to show up
+    let randomText = []; // array to collect random words
+    
+    for(let i = 0; i < numberOfWords; i++) {
+      let randomIndex = Math.floor(Math.random() * wordsArray.length);
+      let randomWord = wordsArray[randomIndex];
+      randomText.push(randomWord); // add word to array
+    }
+    
+    return randomText.join(' '); // join with spaces into a string
+  }
 let testText = "The quick brown fox jumps over the lazy dog.";
 let startTime, endTime;
 function startTest() {
     // Set the test text
+    testText = generateText()
     document.getElementById("inputText").value = testText;
     
     // Reset user input and output
